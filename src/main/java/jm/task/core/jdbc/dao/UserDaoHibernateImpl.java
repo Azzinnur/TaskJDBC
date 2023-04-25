706ec2366f96e35b4c1455f6a45681400b2a1cda
Azzinnur <hermin.chequetet@gmail.com>
2023-04-25 23:12:54 +0300

    Completed 23/04/2023

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,106 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
-
     }
-
 
     @Override
     public void createUsersTable() {
+        try(Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createSQLQuery("CREATE TABLE IF NOT EXISTS `Users` ("
+                    + "  `ID` int NOT NULL AUTO_INCREMENT,\n"
+                    + "  `NAME` varchar(100) NOT NULL,\n"
+                    + "  `LASTNAME` varchar(100) NOT NULL,\n"
+                    + "  `AGE` int DEFAULT 0,\n"
+                    + "  PRIMARY KEY (`ID`)\n"
+                    + ") ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;").executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            System.out.println("Ошибка при создании таблицы!");
+        }
 
     }
 
     @Override
     public void dropUsersTable() {
-
+        try(Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createSQLQuery("DROP TABLE IF EXISTS mydbtest.users;").executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            System.out.println("Ошибка при удалении таблицы!");
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Transaction transaction = null;
+        try(Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            User user = new User(name, lastName, age);
+            session.save(user);
+            transaction.commit();
+            System.out.printf("User с именем %s добавлен в базу данных.\n", name);
+        } catch (HibernateException e) {
+            if(transaction != null) {
+                transaction.rollback();
+            }
+            System.out.printf("Oшибка при добавлении User %s в базу данных!\n", name);
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Transaction transaction = null;
+        try(Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.delete(user);
+            transaction.commit();
+        } catch (HibernateException e) {
+            if(transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println("Ошибка при удалении данных!");
+        }
     }
+
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        Transaction transaction = null;
+        List<User> list = null;
+        try(Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            list = session.createQuery("from User").list();
+            transaction.commit();
+        } catch (HibernateException e) {
+            if(transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println("Ошибка при создании списка Юзеров из базы данных!");
+        }
+        return list;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try(Session session = Util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createSQLQuery("DELETE FROM mydbtest.users;").executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            System.out.println("Ошибка при удалении данных из таблицы!");
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,24 +1,72 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+
 import java.sql.*;
+import java.util.Properties;
 
 public class Util {
 
     private static final String URL = "jdbc:mysql://localhost:3306/mydbtest?useSSL=false";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "Root1234";
+    private static SessionFactory sessionFactory;
+    private static Connection connection;
 
     public static Connection getConnection() {
 
-        Connection connection = null;
-        try {
-            Driver driver = new com.mysql.cj.jdbc.Driver();
-            DriverManager.registerDriver(driver);
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            System.out.println("Ошибка соединения");
+        if (connection == null) {
+            try {
+                Driver driver = new com.mysql.cj.jdbc.Driver();
+                DriverManager.registerDriver(driver);
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            } catch (SQLException e) {
+                System.out.println("Ошибка соединения");
+            }
         }
         return connection;
+    }
+
+    public static SessionFactory getSessionFactory() {
+
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, URL);
+                settings.put(Environment.USER, USERNAME);
+                settings.put(Environment.PASS, PASSWORD);
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.HBM2DDL_AUTO, "");
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(User.class);
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+            } catch (Exception e) {
+                System.out.println("Ошибка SessionFactory");
+            }
+        }
+        return sessionFactory;
+    }
+    public static void close() {
+        try{
+            if (!connection.isClosed()) {connection.close();}
+        } catch(SQLException e) {
+            System.out.println("Cannot close connection");
+        }
     }
 }
 
